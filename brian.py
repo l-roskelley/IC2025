ca831449743c533661502330dbd84fb6a881460e
brisolly <m276078@usna.edu>
2025-03-04 10:19:16 -0500

    work

--- brian.py
+++ brian.py
@@ -1,3 +1,5 @@
 import pandas as pd
 import plotly.express as px
 
+crimes = pd.read_csv('crime_counts.csv', sep=',')
+px.histogram(crimes, x=crimes['OFFENSE'], y=crimes['VALUE'],nbins=24, width=1000, height=600,title="Count Per Type of Crime")
